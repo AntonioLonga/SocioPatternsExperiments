e6cf4ddb4a4433d28b32de4430fbdd73a77cc1bf
AntonioLonga94 <longaantonio@gmail.com>
2019-10-28 18:33:34 +0100

    generate weighted graph 2

--- socioPatterns/utilities.py
+++ socioPatterns/utilities.py
@@ -13,22 +13,59 @@ def plot_weighted_graph(GG,pos, node_size=70):
     plt.axis('off')
     plt.show()
 
-def build_weighted_graph(data):
+
+
+def not_consecutive_duplicates_data(data):
+    i_old = 0
+    j_old = 0
+    new_data = []
+    for t,i,j in data:
+        if not(i == i_old and j==j_old):
+            i_old = i
+            j_old = j
+            new_data.append([t,i,j])
+    new_data = np.array(new_data)
+    return(new_data)
+    
+def build_weighted_graph_2(data,gap=19):
     nodes = individuals(data)
+    data = not_consecutive_duplicates_data(data)
+    graphs,pos = build_graphs(data,gap)
+
     G = nx.complete_graph(nodes)
     for e in G.edges():
         G[e[0]][e[1]]["weight"]=0
-    for t,i,j in data:
-        G[i][j]["weight"] = G[i][j]["weight"] + 1
 
+    for g in graphs:
+        for e in g.edges():
+            G[e[0]][e[1]]["weight"] = G[e[0]][e[1]]["weight"] + 1
     GG = G.copy()
     for e in G.edges():
         if (G[e[0]][e[1]]["weight"] == 0):
             GG.remove_edge(e[0],e[1])
+            
+    return(GG)
+
+
+def build_weighted_graph(data,gap=19):
+    nodes = individuals(data)
+    graphs,pos = build_graphs(data,gap)
+
+    G = nx.complete_graph(nodes)
+    for e in G.edges():
+        G[e[0]][e[1]]["weight"]=0
+
+    for g in graphs:
+        for e in g.edges():
+            G[e[0]][e[1]]["weight"] = G[e[0]][e[1]]["weight"] + 1
+    GG = G.copy()
+    for e in G.edges():
+        if (G[e[0]][e[1]]["weight"] == 0):
+            GG.remove_edge(e[0],e[1])
+            
     return(GG)
 
 def plot_graphs(graphs,splitted_data,pos):
-    
     c=0
     interaction = [len(i) for i in splitted_data]
     for i in graphs:
@@ -41,7 +78,7 @@ def plot_graphs(graphs,splitted_data,pos):
         plt.show()
         c = c+1
         
-def build_graphs(data,gap):
+def build_graphs(data,gap=19):
     graphs = []
     G=nx.Graph()
     nodes = individuals(data)
@@ -57,7 +94,7 @@ def build_graphs(data,gap):
 
     return(graphs,pos)
 
-def split_input_data(data, gap):
+def split_input_data(data, gap=19):
     times = data[:,0]
     pos = times[0]
     chunks = []
@@ -73,5 +110,33 @@ def load_data(path):
         data = [[int(x) for x in line.split()] for line in f]
     data = np.array(data)
     return(data)
+
 def individuals(data):
     return(np.unique(data[:,1:]))
+
+
+
+def plot_normalized_weights_degree(GG):
+    nodes_weights = []
+    degrees = []
+    for node in GG.nodes():
+        degrees.append(GG.degree(node))
+        links = GG.edges(node)
+        node_weights = 0
+        for i in links:
+            node_weights = node_weights + GG[i[0]][i[1]]["weight"]
+
+        nodes_weights.append(node_weights)
+        
+    nodes_weights = np.array(nodes_weights)/np.max(nodes_weights)
+    degrees = np.array(degrees)/np.max(degrees)
+        
+    plt.figure(figsize=(10,5))
+    plt.subplot(121)
+    plt.title("Weights")
+    plt.hist(nodes_weights,bins=50)
+    plt.subplot(122)
+    plt.title("Degree")
+    plt.hist(degrees,bins=50)
+    plt.show()
+    
